9556c5cb1d9f1dc83c3832a583326e0188b224ca
furqanuddin <ffurqanuddin@gmail.com>
2024-07-24 01:25:03 +0500

    Some changes in mit licenses

--- lib/base/contents/app_contents.dart
+++ lib/base/contents/app_contents.dart
@@ -34,7 +34,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
-Copyright (c) 2024 Furqan Uddin
+Copyright (c) 2024 Ghulam Mustafa & Abdul Sattar (Project Owners + Developers) 
+Email : info.wiscomustafa@gmail.com
 """;
 
 
